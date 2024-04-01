f99c0530fe25cd267a9e60c54ab65189cd619755
Bruno Builder <brunofernandotadeu@gmail.com>
2024-03-31 23:26:10 -0300

    Login form global message

--- frontend/src/routes/Admin/ProductForm/index.tsx
+++ frontend/src/routes/Admin/ProductForm/index.tsx
@@ -1,14 +1,22 @@
 import './styles.css';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/forms';
 import { useEffect, useState } from 'react';
 import * as productService from '../../../services/product-service'
+import FormTextArea from '../../../components/FormTextArea';
+import { CategoryDTO } from '../../../models/category';
+import * as categoryService from '../../../services/category-service';
+import FormSelect from '../../../components/FormSelect';
+import { selectStyles } from '../../../utils/select';
 
 
 export default function ProductForm() {
   const params = useParams();
+  const navigate = useNavigate();
   const isEditing = params.productId !== 'create';
+  const [categories, setCategories] = useState<CategoryDTO[]>([]);
+
   const [formData, setFormData] = useState<any>({
     name: {
       value: "",
@@ -39,7 +47,34 @@ export default function ProductForm() {
       type: "text",
       placeholder: "Image",
     },
+    description: {
+      value: "",
+      id: "description",
+      name: "description",
+      type: "text",
+      placeholder: "Descrição",
+      validation: function (value: string) {
+        return /^.{10,}$/.test(value);
+      },
+      message: "A descrição com minimo de 10 caracteres!"
+    },
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      placeholder: "categorias",
+      validation: function (value: CategoryDTO[]) {
+        return value.length > 0;
+      },
+      message: "Escolha ao menos uma categoria!"
+    }
   });
+  useEffect(() => {
+    categoryService.findAllPageRequest()
+      .then(response => {
+        setCategories(response.data)
+      })
+  }, []);
 
   useEffect(() => {
     if (isEditing) {
@@ -61,11 +96,37 @@ export default function ProductForm() {
     setFormData(forms.dirtyAndValidate(formData, name));
   }
 
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if (forms.hasAnyInvalid(formDataValidated)) {
+      setFormData(formDataValidated);
+      return;
+    }
+
+    const requestBody = forms.toValues(formData);
+    if (isEditing) {
+      requestBody.id = params.productId;
+    }
+
+    const request = isEditing
+      ? productService.updateRequest(requestBody)
+      : productService.insertRequest(requestBody)
+
+    request
+      .then(() => {
+        navigate("/admin/products")
+      }).catch(error => {
+        const newInputs = forms.setBackendErros(formData, error.response.data.fieldMessages);
+        setFormData(newInputs);
+      });
+  }
+
   return (
     <main>
       <section id="product-form-section" className="dsc-container">
         <div className="dsc-product-form-container">
-          <form className="dsc-card dsc-form">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
             <h2>Dados do produto</h2>
             <div className="dsc-form-controls-container">
               <div>
@@ -97,6 +158,36 @@ export default function ProductForm() {
                   onTurnDirty={handleInputTurnDirty}
                   onChange={handleInputChange}
                 />
+              </div>
+              <div>
+                <FormSelect
+                  {...formData.categories}
+                  className="dsc-form-control dsc-form-select-container"
+                  styles={selectStyles}
+                  options={categories}
+                  onChange={(obj: any) => {
+                    const newFormData = forms.updateAndValidate(formData, "categories", obj);
+                    setFormData(newFormData);
+                  }}
+                  onTurnDirty={handleInputTurnDirty}
+                  isMulti
+                  getOptionLabel={(obj: any) => obj.name}
+                  getOptionValue={(obj: any) => String(obj.id)}
+                />
+                <div className='dsc-form-error'>
+                  {formData.categories.message}
+                </div>
+              </div>
+              <div>
+                <FormTextArea
+                  {...formData.description}
+                  className="dsc-form-control dsc-textarea"
+                  onTurnDirty={handleInputTurnDirty}
+                  onChange={handleInputChange}
+                />
+                <div className='dsc-form-error'>
+                  {formData.description.message}
+                </div>
               </div>
             </div>
             <div className="dsc-product-form-buttons">
--- frontend/src/utils/forms.ts
+++ frontend/src/utils/forms.ts
@@ -30,12 +30,75 @@ export function toDirty(inputs: any, name: string) {
   return { ...inputs, [name]: { ...inputs[name], dirty: "true" } }
 }
 
+
 export function updateAndValidate(inputs: any, name: string, newValue: any) {
   const dataUpdate = update(inputs, name, newValue);
   return validate(dataUpdate, name);
-} 
+}
 
 export function dirtyAndValidate(inputs: any, name: string) {
   const dataDirty = toDirty(inputs, name);
   return validate(dataDirty, name);
-} 
+}
+
+
+export function toDirtyAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    newInputs[name] = { ...inputs[name], dirty: "true" };
+  }
+  return newInputs;
+}
+
+/*
+export function validationAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    if (inputs[name].validation) {
+      const isInvalid = !inputs[name].validation(inputs[name].value);
+      newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
+    } else {
+      newInputs[name] = { ...inputs[name] }
+    }
+    return newInputs;
+  }
+}
+*/
+
+export function validationAll(inputs: any) {
+  const newInputs: any = {};
+  for (var name in inputs) {
+    if (inputs[name].validation) {
+      const isInvalid = !inputs[name].validation(inputs[name].value);
+      newInputs[name] = { ...inputs[name], invalid: isInvalid.toString() };
+    } else {
+      newInputs[name] = { ...inputs[name] };
+    }
+  }
+  return newInputs;
+}
+
+export function dirtyAndValidateAll(inputs: any) {
+  return validationAll(toDirtyAll(inputs));
+}
+
+
+export function hasAnyInvalid(inputs: any) {
+  for (var name in inputs) {
+    if (inputs[name].dirty === "true" && inputs[name].invalid === "true") {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function setBackendErros(inputs: any, errors: any[]) {
+  const newInputs = { ...inputs };
+  errors.forEach(item => {
+    newInputs[item.fieldName].message = item.message;
+    newInputs[item.fieldName].dirty = "true";
+    newInputs[item.fieldName].invalid = "true";
+  });
+  return newInputs;
+}
+
