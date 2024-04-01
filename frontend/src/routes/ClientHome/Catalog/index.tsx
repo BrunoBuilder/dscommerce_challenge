c224dbaf39e27f02afc6d7b2904a0183997970c3
Bruno Builder <brunofernandotadeu@gmail.com>
2024-03-31 23:16:11 -0300

    catalog next page and cart count

--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -6,22 +6,43 @@ import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/products';
 import * as productService from '../../../services/product-service';
 
+type QueryParams = {
+  page: number;
+  name: string;
+}
+
 
 export default function Catalog() {
+  const [isLastPage, setIsLastPage] = useState(false);
   const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [queryParams, setQueryParams] = useState<QueryParams>({
+    page: 0,
+    name: ""
+  });
 
   useEffect(() => {
-    productService.findAll()
+    productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
-        setProducts(response.data.content);
-      })
-  }, [])
+        const nexPage = response.data.content;
+        setProducts(products.concat(nexPage));
+        setIsLastPage(response.data.last);
+      });
+  }, [queryParams]);
+
+  function handleSearch(searchText: string) {
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 })
+  }
 
 
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
             products.map(
@@ -29,7 +50,12 @@ export default function Catalog() {
             )
           }
         </div>
-        <ButtonNexPage />
+        {
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+            <ButtonNexPage />
+          </div>
+        }
       </section>
     </main>
   );
